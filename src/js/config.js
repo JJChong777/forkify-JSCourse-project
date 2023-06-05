2c656dd7357dfe60cf5663af18a0ad9e6a584fdb
JJChong777 <69834276+JJChong777@users.noreply.github.com>
2023-06-05 19:36:59 +0800

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR KEY>';
+export const KEY = '6428b542-6453-4d85-a0d5-fd5da1e2b848';
 export const MODAL_CLOSE_SEC = 2.5;
